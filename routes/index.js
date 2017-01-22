3d812b200291301bc2904c1ed3cbe9f10dbccd84
Mindframe Education <info@mindframeeducation.com>
2017-01-22 15:53:02 +0000

    Added missing semicolon and reformatted code in index route

--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,57 @@
-var express     = require("express");
-var router      = express.Router();
-var passport    = require("passport");
-var User        = require("../models/user");
-var nodemailer  = require("nodemailer");
-var crypto      = require("crypto");
-var emails      = require("../emails");
-var Student     = require("../models/student");
-var mongoose    = require("mongoose");
-var Entry       = require("../models/entry");
+var express = require("express");
+var router = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
+var nodemailer = require("nodemailer");
+var crypto = require("crypto");
+var emails = require("../emails");
+var Student = require("../models/student");
+var mongoose = require("mongoose");
+var Entry = require("../models/entry");
 
 // Display log-in page
-router.get("/login", isLoggedOut, function(req ,res){
-   res.render("login");
+router.get("/login", isLoggedOut, function(req, res) {
+    res.render("login");
 });
 
 // WORKING VERSION
-router.post("/login", passport.authenticate("local", 
-    {
+router.post("/login", passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: "Invalid username or password",
-    }), function(req, res){
-        req.flash("success", "Welcome back, " + req.user.username);
-        res.redirect("/blogs");
+}), function(req, res) {
+    req.flash("success", "Welcome back, " + req.user.username);
+    res.redirect("/blogs");
 });
 
 // Routes for creating entry from the nav bar
-router.get("/new-entry", isLoggedIn, isAStaff, function(req,res){
-    Student.find({}, function(err, students){
-        if (err){
+router.get("/new-entry", isLoggedIn, isAStaff, function(req, res) {
+    Student.find({}, function(err, students) {
+        if (err) {
             req.flash("error", "Internal error. Please try again");
             res.redirect("/students");
-        } else {
-            res.render("entries/new-from-nav", {students: students});
+        }
+        else {
+            res.render("entries/new-from-nav", {
+                students: students
+            });
         }
     });
 });
 
-router.post("/new-entry", isLoggedIn, isAStaff, function(req,res){
+router.post("/new-entry", isLoggedIn, isAStaff, function(req, res) {
     var student_id = mongoose.Types.ObjectId(req.body.student_id);
-    Student.findById(student_id, function(err, foundStudent){
-        if (err){
+    Student.findById(student_id, function(err, foundStudent) {
+        if (err) {
             req.flash("error", "There is an error: " + err);
             res.redirect("/students");
-        } else {
-            Entry.create(req.body.entry, function(err, entry){
-                if (err){
+        }
+        else {
+            Entry.create(req.body.entry, function(err, entry) {
+                if (err) {
                     req.flash("error", "Error creating entry: " + err);
                     res.redirect("/students");
-                } else {
+                }
+                else {
                     entry.author.id = req.user.id;
                     entry.author.username = req.user.username;
                     entry.save();
@@ -62,62 +66,74 @@ router.post("/new-entry", isLoggedIn, isAStaff, function(req,res){
 });
 
 // Display the register page
-router.get("/register", isLoggedOut, function(req, res){
+router.get("/register", isLoggedOut, function(req, res) {
     res.render("register");
 });
 
-router.post("/register", isLoggedOut, function(req,res){
+router.post("/register", isLoggedOut, function(req, res) {
     var username = req.body.username;
     var password = req.body.password;
     var confirm_password = req.body.confirm_password;
     if (emails.indexOf(username) > -1) {
-        if (password !== confirm_password){
+        if (password !== confirm_password) {
             req.flash("error", "Passwords do not match!");
             return res.redirect("/register");
-        } else {
-            var newUser = new User({username: username, email: username.toLowerCase(), students: [], role: "public"});
-            User.register(newUser, req.body.password, function(err, user){
-                if (err){
+        }
+        else {
+            var newUser = new User({
+                username: username,
+                email: username.toLowerCase(),
+                students: [],
+                role: "public"
+            });
+            User.register(newUser, req.body.password, function(err, user) {
+                if (err) {
                     console.log("There is an error in registration: " + err);
                     req.flash("error", err.message);
                     return res.redirect("/register");
                 }
                 else {
-                    passport.authenticate("local")(req, res, function(){
+                    passport.authenticate("local")(req, res, function() {
                         req.flash("success", "Welcome to Mindframe Education!");
                         return res.redirect("/blogs");
-                    })
+                    });
                 }
             });
         }
-    } else {
+    }
+    else {
         req.flash("error", "Email is not in our system!");
         res.redirect("/register");
     }
 });
 
 // Register page for staff
-router.get("/register/Iyq8UTvzCU/m1ndFrameStaff", isLoggedOut, function(req,res){
+router.get("/register/Iyq8UTvzCU/m1ndFrameStaff", isLoggedOut, function(req, res) {
     res.render("staff_register");
 });
 
 // Register page post request for staff
-router.post("/register/Iyq8UTvzCU/m1ndFrameStaff", isLoggedOut, function(req,res){
+router.post("/register/Iyq8UTvzCU/m1ndFrameStaff", isLoggedOut, function(req, res) {
     var password = req.body.password;
     var confirm_password = req.body.confirm_password;
-    if (password !== confirm_password){
+    if (password !== confirm_password) {
         console.log("Passwords do not match!");
         req.flash("error", "Passwords do not match!");
         return res.redirect("/register/Iyq8UTvzCU/m1ndFrameStaff");
-    } else {
-        var newUser = new User({username: req.body.username, email: req.body.username.toLowerCase(), role: 'user'});
-        User.register(newUser, req.body.password, function(err, user){
-            if (err){
+    }
+    else {
+        var newUser = new User({
+            username: req.body.username,
+            email: req.body.username.toLowerCase(),
+            role: 'user'
+        });
+        User.register(newUser, req.body.password, function(err, user) {
+            if (err) {
                 console.log("There is an error in registration");
                 req.flash("error", err.message);
                 return res.redirect("/register/Iyq8UTvzCU/m1ndFrameStaff");
             }
-            passport.authenticate("local")(req, res, function(){
+            passport.authenticate("local")(req, res, function() {
                 console.log("Staff signed up successfully!");
                 req.flash("success", "Welcome to Mindframe Education!");
                 res.redirect("/blogs");
@@ -127,35 +143,39 @@ router.post("/register/Iyq8UTvzCU/m1ndFrameStaff", isLoggedOut, function(req,res
 });
 
 // CHANGE PASSWORD ROUTES
-router.get("/change-password", function(req, res){
-    if (req.user){
+router.get("/change-password", function(req, res) {
+    if (req.user) {
         res.render("change-password");
-    } else {
+    }
+    else {
         req.flash("error", "Please log in first!");
         res.redirect("/login");
     }
-})
+});
 
-router.post("/change-password", function(req,res){
-    User.findByUsername(req.user.username, function(err, foundUser){
-        if (err){
+router.post("/change-password", function(req, res) {
+    User.findByUsername(req.user.username, function(err, foundUser) {
+        if (err) {
             console.log(err);
             req.flash("error", "No such username found!");
             res.redirect("/change-password");
-        } else {
-            if (req.body.password === req.body.confirm_password){
-                foundUser.setPassword(req.body.password, function(err){
-                    if (err){
+        }
+        else {
+            if (req.body.password === req.body.confirm_password) {
+                foundUser.setPassword(req.body.password, function(err) {
+                    if (err) {
                         console.log("There is an error updating password");
                         req.flash("error", "There is an error. Please try again");
                         res.redirect("/blogs");
-                    } else {
+                    }
+                    else {
                         foundUser.save();
                         req.flash("success", "Password changed successfully!");
                         res.redirect("/blogs");
                     }
-                })
-            } else {
+                });
+            }
+            else {
                 req.flash("error", "Passwords do not match!");
                 res.redirect("back");
             }
@@ -164,37 +184,41 @@ router.post("/change-password", function(req,res){
 });
 
 // Handle logic for logging out
-router.get("/logout", isLoggedIn, function(req, res){
+router.get("/logout", isLoggedIn, function(req, res) {
     req.logout();
     req.flash("success", "You have successfully logged out!");
     res.redirect("/blogs");
 });
 
 // FORGET PASSWORD ROUTES ==========
-router.get("/forget", function(req,res){
+router.get("/forget", function(req, res) {
     res.render("forget");
 });
 
-router.post("/forget", function(req,res,next){
+router.post("/forget", function(req, res, next) {
     var transporter = nodemailer.createTransport({
-        service: "Gmail", 
+        service: "Gmail",
         auth: {
             user: "mfeducationmail",
             pass: "mindframeAdm1n"
         }
-    })
+    });
     // Create a random to token for password reset
-    crypto.randomBytes(20, function(err, buff){
-        if (err){
+    crypto.randomBytes(20, function(err, buff) {
+        if (err) {
             console.log(err);
-        } else {
+        }
+        else {
             var token = buff.toString("hex");
-            User.findOne({email: req.body.username.toLowerCase()}, function(err, user){
-                if (!user){
+            User.findOne({
+                email: req.body.username.toLowerCase()
+            }, function(err, user) {
+                if (!user) {
                     console.log(err); // To prevent the warning
                     req.flash("error", "Username or email not found");
                     res.redirect("/forget");
-                } else {
+                }
+                else {
                     console.log("The user is: " + user);
                     user.resetPasswordToken = token;
                     user.resetPasswordExpires = Date.now() + 36000000; // Password link will expires in 1 hour
@@ -203,18 +227,18 @@ router.post("/forget", function(req,res,next){
                         from: "Mindframe Education",
                         to: user.email,
                         subject: "Password reset",
-                        text: "You are receiving this because you (or someone else) " + 
-                        "have requested a password reset for the username " + req.body.username + 
-                        ". Please click on the following link, or paste this into your browser " + 
-                        "to complete the password reset process: \n" + 
-                        "https://" + req.headers.host + "/reset/" + token + "\n\n" + 
-                        "If you did not request this, please ignore this email and your password will " +
-                        "remain unchanged." + "\n\n\n" +
-                        "Mindframe Dev. team"
+                        text: "You are receiving this because you (or someone else) " +
+                            "have requested a password reset for the username " + req.body.username +
+                            ". Please click on the following link, or paste this into your browser " +
+                            "to complete the password reset process: \n" +
+                            "https://" + req.headers.host + "/reset/" + token + "\n\n" +
+                            "If you did not request this, please ignore this email and your password will " +
+                            "remain unchanged." + "\n\n\n" +
+                            "Mindframe Dev. team"
                     };
-                    
-                    transporter.sendMail(mailOptions, function(err, info){
-                        if (err){
+
+                    transporter.sendMail(mailOptions, function(err, info) {
+                        if (err) {
                             return console.log("Error: " + err);
                         }
                         console.log("Success!");
@@ -222,64 +246,81 @@ router.post("/forget", function(req,res,next){
                         res.redirect("/blogs");
                     });
                 }
-            })
+            });
         }
     });
-    
+
 });
 // =================================
 
 // PASSWORD RESET ROUTE =================
-router.get("/reset/:token", function(req,res){
-    User.findOne({resetPasswordToken: req.params.token, resetPasswordExpires: {$gt: Date.now()}}, function(err, user){
-        if (!user){ // If no such user exists
+router.get("/reset/:token", function(req, res) {
+    User.findOne({
+        resetPasswordToken: req.params.token,
+        resetPasswordExpires: {
+            $gt: Date.now()
+        }
+    }, function(err, user) {
+        if (!user) { // If no such user exists
             console.log(err);
             req.flash("error", "The reset password link is invalid or has expired");
             res.redirect("/blogs");
-        } else {
-            res.render("reset", {token: req.params.token});
+        }
+        else {
+            res.render("reset", {
+                token: req.params.token
+            });
         }
     });
 });
 
-router.post("/reset/:token", function(req,res){
-    User.findOne({resetPasswordToken: req.params.token, resetPasswordExpires: {$gt: Date.now()}}, function(err, user){
-        if (err){
+router.post("/reset/:token", function(req, res) {
+    User.findOne({
+        resetPasswordToken: req.params.token,
+        resetPasswordExpires: {
+            $gt: Date.now()
+        }
+    }, function(err, user) {
+        if (err) {
             console.log("There is an error: " + err);
             req.flash("error", "The reset password link is invalid or has expired");
             res.redirect("/blogs");
-        } else {
-            if (req.body.password === req.body.confirm_password){
+        }
+        else {
+            if (req.body.password === req.body.confirm_password) {
                 // Nullify the reset password fields. Making them undefined will have
                 // them now showing up when showing the database
                 user.resetPasswordToken = undefined;
                 user.resetPasswordExpires = undefined;
-                user.setPassword(req.body.password, function(err){
-                    if (err){
+                user.setPassword(req.body.password, function(err) {
+                    if (err) {
                         console.log("Error: " + err);
                         res.redirect("/blogs");
-                    } else {
+                    }
+                    else {
                         user.save();
                         req.flash("success", "Password reset successfully! Please login with your new password!");
                         res.redirect("/login");
                     }
-                })
-            } else {
+                });
+            }
+            else {
                 req.flash("error", "Password do not match! Please try again!");
                 res.redirect("/login");
             }
         }
-    })
-})
+    });
+});
 
 // ======================================
 
 // Check if there is a user currently logged in
-function isLoggedOut(req,res,next){
-    if (req.user){
+function isLoggedOut(req, res, next) {
+    if (req.user) {
         req.flash("error", "Please log out first");
         res.redirect("/blogs");
-    } else {
+    }
+    else {
         next();
     }
 }
@@ -287,8 +328,8 @@ function isLoggedOut(req,res,next){
 // ==========================================
 // Function to check if the user is logged in
 // ==========================================
-function isLoggedIn(req, res, next){
-    if (req.isAuthenticated()){
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
         return next();
     }
     req.flash("error", "You are not logged in!");
@@ -297,20 +338,21 @@ function isLoggedIn(req, res, next){
 
 
 // Function to check if the user is a staff member
-function isAStaff(req,res,next){
-    if(req.isAuthenticated()){ // If the user is logged in
-        if (req.user.hasAccess('user')){ // If the user is a staff member
+function isAStaff(req, res, next) {
+    if (req.isAuthenticated()) { // If the user is logged in
+        if (req.user.hasAccess('user')) { // If the user is a staff member
             next();
-        } else {
+        }
+        else {
             req.flash("error", "Permission denied!");
             res.redirect("/blogs");
         }
     }
 }
 
-router.get("/progress", function(req,res){
+router.get("/progress", function(req, res) {
     res.render("progress");
-})
+});
 
 module.exports = router;
 
@@ -321,4 +363,3 @@ module.exports = router;
 //       req.flash("success", "Welcome back, " + req.user.username);
 //       res.redirect("/blogs");
 //   });
-  
