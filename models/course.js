2c6881b28a65c92f16b296beceff48319e06fee8
Mindframe Education <info@mindframeeducation.com>
2017-02-06 15:41:41 +0000

    Add a virtual that calculates the course's progress and change student's showpage to utilize the new virtual field

--- models/course.js
+++ models/course.js
@@ -10,4 +10,16 @@ var courseSchema = mongoose.Schema({
     ]
 });
 
+// A virtual field (progress) that keeps track of the current course's progress
+courseSchema.virtual("progress").get(function(){
+    var total = this.units.length;
+    var completed = 0;
+    for (var i = 0; i < this.units.length; i++){
+        if (this.units[i].completed) {
+            completed++;
+        }
+    }
+    return completed/total*100;
+});
+
 module.exports = mongoose.model("Course", courseSchema);
