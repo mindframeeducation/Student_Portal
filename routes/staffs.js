e81347b7e4d5e2258ff3157432e756aabb3800a4
Mindframe Education <info@mindframeeducation.com>
2017-02-23 20:09:42 +0000

    Removed admin as an option when editing user role

--- routes/staffs.js
+++ routes/staffs.js
@@ -4,7 +4,7 @@ var express = require("express"),
     middlewareObj = require("../middleware");
 
 // Route to display the index page for staff management
-router.get("/staff", function(req,res){
+router.get("/staff", middlewareObj.isLoggedIn, middlewareObj.isAdmin, function(req,res){
     User.find({}, function(err, users){
         if (err){
             console.log("Error" + err);
@@ -18,7 +18,7 @@ router.get("/staff", function(req,res){
 });
 
 // Route to update a staff member
-router.put("/staff/:id", function(req,res){
+router.put("/staff/:id", middlewareObj.isLoggedIn, middlewareObj.isAdmin, function(req,res){
     if (req.body.user.role === "admin"){
         req.flash("error", "You cannot assign admin role!");
         return res.redirect("back");
@@ -34,7 +34,7 @@ router.put("/staff/:id", function(req,res){
     });
 });
 
-router.delete("/staff/:id", function(req,res){
+router.delete("/staff/:id", middlewareObj.isLoggedIn, middlewareObj.isAdmin, function(req,res){
     User.findByIdAndRemove(req.params.id, function(err){
         if (err){
             req.flash("error", "Error");
